d0c194795949184c2d87afa707fb267dc29d8520
JAPJEET SINGH <95476734+JAPJEET01@users.noreply.github.com>
2023-11-01 08:54:31 +0000

    Update rtspclient.py

--- rtspclient.py
+++ rtspclient.py
@@ -1,38 +1,23 @@
-import cv2
-import pyaudio
+import sounddevice as sd
+import socket
 import numpy as np
 
-if __name__ == "__main__":
-    cap = cv2.VideoCapture("rtsp://<server_ip>:8554/live")
+# Configuration
+receiver_ip = '192.168.1.101'  # Laptop 1's IP address
+receiver_port = 12345
+sample_rate = 44100
 
-    # Audio configuration
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
+# Create a socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((receiver_ip, receiver_port))
 
-    audio_stream = pyaudio.PyAudio()
-    audio_output = audio_stream.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        output=True,
-        frames_per_buffer=CHUNK
-    )
+# Initialize audio stream
+def audio_callback(outdata, frames, time, status):
+    if status:
+        print(status, flush=True)
+    data, addr = sock.recvfrom(outdata.nbytes)
+    outdata[:] = np.frombuffer(data, dtype=np.int16)
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Extract audio from the frame (you may need to adapt this part)
-        audio_data = frame
-
-        # Play audio
-        audio_output.write(audio_data)
-
-    cap.release()
-    cv2.destroyAllWindows()
-    audio_output.stop_stream()
-    audio_output.close()
-    audio_stream.terminate()
+with sd.OutputStream(callback=audio_callback, channels=2, samplerate=sample_rate):
+    print(f"Receiving audio from {receiver_ip}:{receiver_port}")
+    sd.sleep(-1)
